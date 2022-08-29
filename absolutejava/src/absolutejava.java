098f8188f5020053275a19772412c0dd24a5a135
huubexel <huubexel@hotmail.com>
2022-08-29 22:23:17 +0200

    Switch in java

--- absolutejava/src/absolutejava.java
+++ absolutejava/src/absolutejava.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.File;
+import java.nio.file.Paths;
 
 public class absolutejava {
     public static void main(String[] args) {
-        Scanner fileIn = null;
-        try {
-            fileIn = new Scanner(new FileInputStream("./file.java"));
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found.");
-            System.exit(0);
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter number of ice cream flavors:");
+        int numberOfFlavors = keyboard.nextInt( );
+
+        switch (numberOfFlavors) {
+            case 32 -> System.out.println("Nice selection.");
+            case 2, 3, 4 -> {
+                System.out.println(numberOfFlavors + "flavors");
+                System.out.println("is acceptable.");
+            }
+            default -> {
+                System.out.println("I didn't plan for");
+                System.out.println(numberOfFlavors + " flavors.");
+            }
         }
     }
 }
